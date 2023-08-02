f03149e060a1a99da47ca3837f29d0f3604eefae
Hamilton Chevez <hchev001@gmail.com>
2023-08-02 17:46:06 -0400

    Setup Config:
    Changelog:
    - updated tailwind config to include some of design colors
    - removed everything from main
    - WIP: header floating menu bar up top

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,41 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      plomo: {
+        100: "#F6F6F6",
+        200: "#E4E4E4",
+        300: "#C9C9C9",
+        400: "#B7B7B7",
+        500: "#A5A5A5",
+        600: "#878787",
+        700: "#6A6A6A",
+        800: "#4C4C4C",
+        900: "#2F2F2F",
+      },
+    },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
+};
